987da18c52a5d5ee3a0f905ac3f868fd2c95c093
Ngoc Man <nguyen.ngoc.man@sun-asterisk.com>
2020-07-31 18:51:34 +0700

    Chapter 4, 5: Rails-flavored Ruby, Filling in the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basic.
+  def full_title page_title = "", base_title = ""
+    if page_title.blank?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
