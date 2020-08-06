db2a37e1a7eebe9b1a54eb9ac137dafbb0c50dcf
Ngoc Man <nguyen.ngoc.man@sun-asterisk.com>
2020-08-06 18:23:45 +0700

    Chapter 8: Basic Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,17 @@ class User < ApplicationRecord
   has_secure_password
 
   before_save :downcase_email
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create string, cost: cost
+  end
+
   private
+
   def downcase_email
     email.downcase!
   end
